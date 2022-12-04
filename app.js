3fb9b3d0c62d9533b2bb6f2f4f7b73382c5ff194
amer-tech <alicalan01@gmail.com>
2022-12-04 12:38:09 +0100

    added ui

--- app.js
+++ app.js
@@ -1,52 +1,90 @@
+const rockButton  = document.querySelector("#rock");
+const paperButton  = document.querySelector("#paper");
+const scissorsButton  = document.querySelector("#scissors");
+const resetButton = document.querySelector("#reset")
+const playerSpan = document.querySelector("#playerSpan");
+const computerSpan = document.querySelector("#computerSpan");
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
-    const allComputerChoices = ['rock', 'paper', 'scissors'];
-    return allComputerChoices[Math.floor(Math.random() * 3)]
+    const allComputerChoices = ["rock", "paper", "scissors"];
+    return allComputerChoices[Math.floor(Math.random() * 3)];
+}
+
+function toggleButtons(bool) {
+    rockButton.disabled = bool;
+    paperButton.disabled = bool;
+    scissorsButton.disabled = bool;
 }
 
 function playRound(playerSelection, computerSelection) {
-    playerSelection.toLowerCase()
-    computerSelection = getComputerChoice()
-    if (playerSelection === `rock` && computerSelection === `rock` ||
-        playerSelection === `paper` && computerSelection === `paper` ||
-        playerSelection === `scissors` && computerSelection === `scissors`) {
-        return `EQUAL`
+    playerSelection.toLowerCase();
+    computerSelection = getComputerChoice();
+    if (playerSelection === "rock" && computerSelection === "rock" ||
+        playerSelection === "paper" && computerSelection === "paper" ||
+        playerSelection === "scissors" && computerSelection === "scissors") {
+        console.log("EQUAL");
     }
-    else if (playerSelection === `rock` && computerSelection === `scissors` ||
-        playerSelection === `paper` && computerSelection === `rock` ||
-        playerSelection === `scissors` && computerSelection === `paper`) {
-        return `WIN`
+    else if (playerSelection === "rock" && computerSelection === "scissors" ||
+        playerSelection === "paper" && computerSelection === "rock" ||
+        playerSelection === "scissors" && computerSelection === "paper") {
+        playerScore++; 
+        playerSpan.innerText = playerScore;
+        console.log("WIN");
     }
-    else if (playerSelection === `paper` && computerSelection === `scissors` ||
-        playerSelection === `rock` && computerSelection === `paper` ||
-        playerSelection === `scissors` && computerSelection === `rock`) {
-        return 'LOSE'
+    else if (playerSelection === "paper" && computerSelection === "scissors" ||
+        playerSelection === "rock" && computerSelection === "paper" ||
+        playerSelection === "scissors" && computerSelection === "rock") {
+        computerScore++; 
+        computerSpan.innerText = computerScore;
+        console.log("LOSE");
+    }
+    if (playerScore == 5) {
+        alert("player WON!");
+        toggleButtons(true);
+    }
+    if (computerScore == 5) {
+        alert("computer WON!");
+        toggleButtons(true);
     }
 }
 
-function game() {
-    let playerScore = 0
-    let computerScore = 0
-    for (i = 0; i < 5; i++) {
-        let userChoice = playRound(prompt(`CHOSEE NOW`).toLowerCase())
-        if (userChoice === `EQUAL`) {
-            console.log(`You are equal`)
-        }
-        else if (userChoice === `WIN`) {
-            console.log(`You WIN`)
-            playerScore++
-        }
-        if (userChoice === `LOSE`) {
-            console.log(`You LOSE`)
-            computerScore++
-        }
-    }
-    if (playerScore > computerScore) {
-        console.log(`YOU WINNNNNNNNN`)
-    }
-    else if (playerScore < computerScore) {
-        console.log(`YOU LOST BICH`)
-    }
-    if (playerScore === computerScore) {
-        console.log(`It is equall, lame..`)
-    }
-}
+rockButton.addEventListener("click", () => playRound("rock"));
+paperButton.addEventListener("click", () => playRound("paper"))
+scissorsButton.addEventListener("click", () => playRound("scissors"))
+resetButton.addEventListener("click", () => {
+    toggleButtons(false);
+    computerSpan.innerText = 0;
+    playerSpan.innerText = 0;
+    playerScore = 0;
+    computerScore = 0;
+})
+
+// function game() {
+//     let playerScore = 0
+//     let computerScore = 0
+//     for (i = 0; i < 5; i++) {
+//         let userChoice = playRound(prompt(`CHOSEE NOW`).toLowerCase())
+//         if (userChoice === `EQUAL`) {
+//             console.log(`You are equal`)
+//         }
+//         else if (userChoice === `WIN`) {
+//             console.log(`You WIN`)
+//             playerScore++
+//         }
+//         if (userChoice === `LOSE`) {
+//             console.log(`You LOSE`)
+//             computerScore++
+//         }
+//     }
+//     if (playerScore > computerScore) {
+//         console.log(`YOU WINNNNNNNNN`)
+//     }
+//     else if (playerScore < computerScore) {
+//         console.log(`YOU LOST`)
+//     }
+//     if (playerScore === computerScore) {
+//         console.log(`It is equall, lame..`)
+//     }
+// }
